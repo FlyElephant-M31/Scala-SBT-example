a2f42b9102f578e93916512f235f1d20ebd35036
Slava Schmidt <slavaschmidt@gmx.de>
2016-04-08 07:41:03 +0200

    refactor: chart appearance

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,43 +1,33 @@
-import scala.io.Source
-import scala.util.control.Breaks._
-import math.abs
-import sys.process._
+import java.awt.BasicStroke
 
-import breeze.linalg._
-import breeze.plot._
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer
+import org.jfree.data.xy.DefaultXYDataset
+import org.jfree.util.ShapeUtilities
+
+import scala.io.Source
+import scala.math.abs
 import scalax.chart.api._
 
 
 object GradientDescent {
 
   def getData = {
-    var mileage:List[Float] = Nil
-    var price:List[Float] = Nil
-
-    try {
-      for(line <- Source.fromFile("Data.csv").getLines()) {
-        val tab = line.split(",")
-        if (tab.length == 2 && line != "mileage(in km),price(in euro)") {
-          mileage = mileage ::: List(tab(0).toFloat / 1000)
-          price = price ::: List(tab(1).toFloat / 1000)
-        }
-      }
-    } catch {
-      case t:Throwable => {
-        println("Unable to get data, please make sure that you have access to Dataset/Data.csv");
-        System.exit(0)
-      }
-    }
-    (mileage, price)
+    val mileagesAndPrices = for {
+      line <- Source.fromFile("Data.csv").getLines()
+      tab = line.split(",") if tab.length == 2 && ! line.contains("mileage")
+      m = tab(0).toFloat / 1000f
+      p = tab(1).toFloat / 1000f
+    } yield (m, p)
+    mileagesAndPrices.toList.unzip
   }
 
-  def gradientDescent(mileage:List[Float], price:List[Float], theta0:Float, theta1:Float) : (Float, Float) = {
+  def gradientDescent(mileage:List[Float], price:List[Float], theta0:Float, theta1:Float) : (Double, Double) = {
     val learningRate = 0.0001f
-    val tmpTheta0 = learningRate * (Function.getSum0(mileage, price, theta0, theta1) / (price.length).toFloat)
-    val tmpTheta1 = learningRate * (Function.getSum1(mileage, price, theta0, theta1) / (price.length).toFloat)
+    val tmpTheta0 = learningRate * (Function.getSum0(mileage, price, theta0, theta1) / price.length.toFloat)
+    val tmpTheta1 = learningRate * (Function.getSum1(mileage, price, theta0, theta1) / price.length.toFloat)
 
     if (abs(tmpTheta0) < 0.000001 && abs(tmpTheta1) < 0.000001) (theta0 * 1000, theta1)
-    else gradientDescent(mileage, price, (theta0 - tmpTheta0), (theta1 - tmpTheta1))
+    else gradientDescent(mileage, price, theta0 - tmpTheta0, theta1 - tmpTheta1)
   }
 
   def main(av:Array[String]) {
@@ -48,23 +38,22 @@ object GradientDescent {
     val res = (theta1 * ln.toFloat) + theta0
     println(s"This car worth ${res.toInt} euro")
 
-    val estimate = mileage.map(x => (theta1 * (x * 1000)) + theta0)
+    val xs = mileage map { _.toDouble * 1000 }
+    val ys = price map { _.toDouble * 1000 }
+    val estimates = xs map { theta1 * _ + theta0 }
 
-   /* val f = Figure()
-    val p = f.subplot(0)
-    p += plot(mileage.map(x => x * 1000), price.map(x => x * 1000), '.')
-    p += plot(mileage.map(x => x * 1000), estimate)
-    p.xlabel = "mileage (in km)"
-    p.ylabel = "price (in euro)"
+    val dataSet = new DefaultXYDataset()
+    dataSet.addSeries("Prices", Array(xs.toArray, ys.toArray))
+    dataSet.addSeries("Estimates", Array(xs.toArray, estimates.toArray))
 
-    f.saveas("dataGraph.png")
-*/
-    val data = for (i <- 1 to 5) yield (5*i,i)
-    println(data)
-    val chart = XYLineChart(data)
+    val chart = XYLineChart(dataSet)
+    val renderer = new XYLineAndShapeRenderer()
+    renderer.setSeriesStroke(1, new BasicStroke(3f))
+    val shape = ShapeUtilities.createDownTriangle(50f)
+    renderer.setBaseShape(shape)
+    chart.plot.setRenderer(renderer)
     chart.saveAsPNG("chart.png")
-
-    }
+  }
 
 
 }
